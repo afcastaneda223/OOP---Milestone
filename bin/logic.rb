68a116e218408f2505fd5c8d3c38c508fe343598
afcastaneda223 <af.castaneda223@gmail.com>
2020-08-14 12:57:45 -0300

    updating files with structure ideas:

---
+++ bin/logic.rb
@@ -0,0 +1,14 @@
+case winner
+when (board[0] && board[1] && board[2]) || (board[3] && board[4] && board[5]) || (board[6] && board[7] && board[8])|| (board[0] && board[4] && board[8]) (board[3] && board[4] && board[6]) == "X"
+    return "player one wins" 
+when (board[0] && board[1] && board[2]) || (board[3] && board[4] && board[5]) || (board[6] && board[7] && board[8])|| (board[0] && board[4] && board[8]) (board[3] && board[4] && board[6]) == "O"
+    return "Player two wins"
+else 
+    return "tie"
+end
+
+def initialize(name,symbol)
+    @@name = name
+    @@symbol = symbol
+end
+end
--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,87 @@
 #!/usr/bin/env ruby
+# what do you need from player?
+require 'colorize'
+#classes
+class Player
+    def initialize(name,symbol)
+        @name = name
+        @symbol = symbol
+    end
+    def current_user
+        @name
+    end
+end
+
+board = ["1", "2", "3","4", "5", "6","7", "8", "9"]
+
+def grid(board)
+    puts "#{board[0]} | #{board[1]} | #{board[2]}"
+    puts "---------"
+    puts "#{board[3]} | #{board[4]} | #{board[5]}"
+    puts "---------"
+    puts "#{board[6]} | #{board[7]} | #{board[8]}"
+end
+#main
+puts "Welcome to Tic Tac Toe game".blue
+
+puts "Please enter name for player X".red
+player_one = Player.new(gets.chomp, "X")
+
+puts "Please enter name for player O".green
+player_two = Player.new(gets.chomp, "O")
+
+puts "Welcome ".white + "#{player_one.current_user}".red + " your symbol is X".white
+puts "Welcome ".white + "#{player_two.current_user}".green +  " your symbol is O".white
+
+# Play time
+
+puts "#{player_one.current_user}".red + " mark your X in a number".white
+puts grid(board)
+pl_one_x = gets.chomp.to_i
+
+if (1..9).any?(pl_one_x) #match board && !"X" || "O"
+    board[pl_one_x - 1] = "X"
+    puts grid(board)
+else
+    puts "Enter a number between 1 and 9"
+end
+
+puts "#{player_two.current_user}".green + " mark your O in a number".white
+puts grid(board)
+pl_two_o = gets.chomp.to_i
+
+if (1..9).any?(pl_one_x) && != "X"
+    board[pl_two_o - 1] = "O"
+    puts grid(board)
+else
+    puts "Enter a number between 1 and 9 and diferent from X"
+end
+
+
+=begin
+
+# if move is valid return  change of player else return 
+puts "Invalid move"
+
+# Player change
+puts "Player O".green + " make your move".white
+puts grid(board)
+
+# if move is valid return  change of player else return 
+puts "Invalid move"
+
+1st move
+other moves <=9
+win? 
+valid? 
+
+input de jugardor = (1..9) &&  bard.match(input)
+
+
+while !win? valid?
+count 
+    move
+
+else win? 
+    retunt winer
+=end
