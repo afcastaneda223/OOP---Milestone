c8a6c831da1641d498351b96235b195ea32f9b95
afcastaneda223 <af.castaneda223@gmail.com>
2020-08-14 12:43:46 -0300

    updated rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -5,7 +5,7 @@ puts 'Welcome to Tic Tac Toe game'.blue
 puts 'Please enter name for player X'.red
 player_one = gets.chomp
 puts 'Please enter name for player O'.green
-player_one = gets.chomp
+player_two = gets.chomp
 board = %w[1 2 3 4 5 6 7 8 9]
 def grid(board)
   puts "#{board[0]} | #{board[1]} | #{board[2]}"
@@ -15,12 +15,12 @@ def grid(board)
   puts "#{board[6]} | #{board[7]} | #{board[8]}"
 end
 # Play time
-puts 'Player X'.red + ' make your move'.white
+puts player_one.to_s.red + ' make your move'.white
 puts grid(board)
 # if move is valid return marked board and change player else return invalid move
 puts 'Invalid move'
 # Player change
-puts 'Player O'.green + ' make your move'.white
+puts player_two.to_s.green + ' make your move'.white
 # Marked board
 puts grid(board)
 # if move is valid return marked board and change player else return invalid move
