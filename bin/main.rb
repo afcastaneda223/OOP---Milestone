2a37432d8452fb75c35688c65cff5f7393a5084a
afcastaneda223 <af.castaneda223@gmail.com>
2020-08-14 12:32:45 -0300

    added user interface messages;

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,30 @@
 #!/usr/bin/env ruby
+# what do you need from player?
+require 'colorize'
+puts 'Welcome to Tic Tac Toe game'.blue
+puts 'Please enter name for player X'.red
+player_one = gets.chomp
+puts 'Please enter name for player O'.green
+player_one = gets.chomp
+board = %w[1 2 3 4 5 6 7 8 9]
+def grid(board)
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts '---------'
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts '---------'
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
+end
+# Play time
+puts 'Player X'.red + ' make your move'.white
+puts grid(board)
+# if move is valid return marked board and change player else return invalid move
+puts 'Invalid move'
+# Player change
+puts 'Player O'.green + ' make your move'.white
+# Marked board
+puts grid(board)
+# if move is valid return marked board and change player else return invalid move
+puts 'Invalid move'
+# Repeat until win or tie
+puts 'winner is'.white 'player X'.red || 'Player O'.green
+# End of the game and start again
