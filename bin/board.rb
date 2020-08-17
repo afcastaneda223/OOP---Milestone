a70ebdb9874ee81e6f3b8d2452e90e0792a0ed73
Rocio01 <zrmartinezg@gmail.com>
2020-08-17 09:38:09 -0500

    Add method for winner board.rb

--- bin/board.rb
+++ bin/board.rb
@@ -1,19 +1,64 @@
+board = %w[O 1 2 O O 5 X X O]
+# def valid_move (a, b)
+#     a.any?{|x| x == b.to_s}
+# end
+
+#  puts  valid_move(board, 4)
+#  puts  valid_move(board, 1)
+#  puts  valid_move(board, 8)
+#  puts  valid_move(board, 9)
+
+# #  if valid_move(board,pl_one_x)
+# #     #actualizar board
+# #  else
+#     return "invalid move"
+#  end
+#  
+
+# # def winner(x)
+# #     if  (x[0] && x[3] && x[6]) || (x[1] && x[4] && x[7]) || (x[2] && x[5] && x[8]) == "X"
+# #         return "player one wins"
+# #     elsif (x[0] && x[1] && x[2]) || (x[3] && x[4] && x[5]) || (x[6] && x[7] && x[8])== "X"
+# #         return "player one wins"
+# #     elsif (x[0] && x[4] && x[8]) (x[2] && x[4] && x[6] ) == "X"
+# #         return "player one wins"
+# #     else (x[0] && x[3] && x[6]) || (x[1] && x[4] && x[7]) || (x[2] && x[5] && x[8]) || (x[0] && x[1] && x[2]) || (x[3] && x[4] && x[5]) || (x[6] && x[7] && x[8]) || (x[0] && x[4] && x[8]) (x[3] && x[4] && x[6]) == "O"
+# #         return "Player two wins"
+# #     end
+# # end
+
+def winner(x)
   
-board = ["1", "2", "3","X", "5", "6","7", "O", "9"]
-def valid_move (a, b)
-    a.any?{|x| x == b.to_s}
+  if x[0] == "X" && x[1] == "X" && x[2] == "X"  
+    return 'player one wins'   
+  else 
+    return false
+  end
 end
 
- puts  valid_move(board, 4)
- puts  valid_move(board, 1)
- puts  valid_move(board, 8)
- puts  valid_move(board, 9)
+puts winner(board)
 
- if valid_move(board,pl_one_x)
-    #actualizar board
- else
-    return "invalid move"
- end
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [0, 4, 8]
+]
 
-def winner(tabl,combinations)
-    tabl.any? 
+def wins(x)
+  result="No winner"
+  WIN_COMBINATIONS.each do |w|
+    if x[w[0]] == "X" && x[w[1]] == "X" && x[w[2]] == "X" 
+       result="Player X wins"
+    elsif x[w[0]] == "O" && x[w[1]] == "O"  && x[w[2]] == "O"
+        result= "Player O wins"
+    end
+  end
+  return result
+end
+   
+puts wins(board)
