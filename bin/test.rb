666ce806b95f407ee70d0424449360ab07b93019
Rocio01 <zrmartinezg@gmail.com>
2020-08-18 12:37:43 -0500

    Add play method in test.rb

--- bin/test.rb
+++ bin/test.rb
@@ -21,17 +21,49 @@ end
 
 def game 
 
+  # def current_player
+  #   case  @tie_counter 
+  #   when 0
+  #     player_one
+  #   when 1
+  #     player_two
+  #   when 2
+  #     player_one
+  #   when 3
+  #     player_two
+  #   when 4
+  #     player_one
+  #   when 5
+  #     player_two
+  #   when 6
+  #     player_one
+  #   when 7
+  #     player_two
+  #   when 8
+  #     player_one
+              
+  # end
 
-def play 
-    tie_counter = 0
-    while !win && tie_counter < 9
+  @tie_counter = 0  
+def play    
+      while  @tie_counter < 9 
         puts @board
-        if valid_move 
-            tie_counter += 1
-            board[valid_move - 1] = "X".red
-            change_player
+      if !win && valid_move.is_a? (Numeric)
+        if @tie_counter == 8 
+        return puts "Tie game"
+        elsif @tie_counter.even?
+             board[valid_move - 1] = "X".red
+             tie_counter += 1
+        elsif @tie_counter.odd?              
+             board[valid_move - 1] = "O".green
+             tie_counter += 1
         end
-    end
+      else  win 
+          return winners
+        
+        end
+            end
+          end
 end
 puts valid_move
 
